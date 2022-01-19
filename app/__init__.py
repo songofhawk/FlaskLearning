373998202a2b56648b41113a9bd8771050481de9
songofhawk <songofhawk@gmail.com>
2022-01-19 14:47:47 +0800

    Initial commit

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+
+from flask_login import LoginManager
+from flask_openid import OpenID
+import os
+from config import basedir
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
+from app import views, models
+
+
+
+
