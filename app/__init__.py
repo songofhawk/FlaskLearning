cf2add5ae8fa392214d7bba758c78e8b7a945d4a
songofhawk <songofhawk@gmail.com>
2022-01-20 16:05:40 +0800

    1.修复文件日志路径

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+import config
+
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
@@ -33,8 +35,8 @@ if not app.debug:
 if not app.debug:
     import logging
     from logging.handlers import RotatingFileHandler
-
-    file_handler = RotatingFileHandler('tmp/microblog.log', 'a', 1 * 1024 * 1024, 10)
+    '''os.path.join的时候，tmp前面不能有/，要不会被认为是从根目录开始的绝对路径，从而抛弃前面所有的参数'''
+    file_handler = RotatingFileHandler(os.path.join(config.basedir, 'tmp/microblog.log'), 'a', 1 * 1024 * 1024, 10)
     file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
     app.logger.setLevel(logging.INFO)
     file_handler.setLevel(logging.INFO)
